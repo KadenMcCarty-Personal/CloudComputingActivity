e400d207b88eb4b06a9b76ff8985b67f6451a0ab
BackendDev <kadenmccarty1216@gmail.com>
2026-01-26 20:54:19 -0700

    added security config and UserDetailsService.java

---
+++ src/main/java/com/kaden/products/data/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.kaden.products.data;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+}
