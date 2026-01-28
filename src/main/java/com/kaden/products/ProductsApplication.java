d63b9046451ffd7743bccb0a11d110b8a7e4caaf
BackendDev <kadenmccarty1216@gmail.com>
2026-01-27 18:49:19 -0700

    ProductsApplicationl.java was using spring jdbc and other parts of the app were using spring JPA

--- src/main/java/com/kaden/products/ProductsApplication.java
+++ src/main/java/com/kaden/products/ProductsApplication.java
@@ -2,10 +2,10 @@ package com.kaden.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJdbcRepositories //(basePackages = "com.kaden.products.data")
+@EnableJpaRepositories
 public class ProductsApplication {
 
 	public static void main(String[] args) {
