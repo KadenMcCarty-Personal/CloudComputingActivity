f84cedc0c965262ec52e5ba60dacdb35a14c4bf9
BackendDev <kadenmccarty1216@gmail.com>
2026-01-27 22:26:10 -0700

    now goes to correct page after being logged in

--- src/main/java/com/kaden/products/config/SecurityConfig.java
+++ src/main/java/com/kaden/products/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             )
             .formLogin(form -> form
                 .loginPage("/login")
-                .defaultSuccessUrl("/", true)
+                .defaultSuccessUrl("/orders", true)
                 .permitAll()
             )
             .logout(logout -> logout
