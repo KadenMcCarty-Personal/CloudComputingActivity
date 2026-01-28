224d81dbae63357059d21ade7fea5fbfcbb941e5
BackendDev <kadenmccarty1216@gmail.com>
2026-01-27 19:06:57 -0700

    added AuthController.java and code for it

---
+++ src/main/java/com/kaden/products/controllers/AuthController.java
@@ -0,0 +1,45 @@
+package com.kaden.products.controllers;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kaden.products.data.UserEntity;
+import com.kaden.products.data.UsersRepository;
+
+@Controller
+public class AuthController {
+
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
+        this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new UserEntity());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistration(@RequestParam String username, @RequestParam String password) {
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole("ROLE_USER");
+        user.setEnabled(true);
+        usersRepository.save(user);
+        return "redirect:/login?registered";
+    }
+}
